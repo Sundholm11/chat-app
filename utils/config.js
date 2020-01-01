85d5117f0cfc9e1681f2c1e7c86c177df2a13504
Daniel Sundholm <daniel.j.sundholm@utu.fi>
2020-01-01 18:43:16 +0200

    config tweaks

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,10 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
+if (PORT === undefined) {
+    PORT = 3003
+}
+
 if (process.env.NODE_ENV === 'test') {
     MONGODB_URI = process.env.TEST_MONGODB_URI
 }
