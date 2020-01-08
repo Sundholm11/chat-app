61ec2d83891095142c147519782baf817472d955
Daniel Sundholm <daniel.j.sundholm@utu.fi>
2020-01-09 01:17:36 +0200

    Socket endpoint adjustments

--- client/src/reducers/socketReducer.js
+++ client/src/reducers/socketReducer.js
@@ -3,7 +3,7 @@ import socketIOClient from 'socket.io-client'
 const socketReducer = (state = null, action) => {
 	switch(action.type) {
 		case 'CONNECT_SOCKET':
-			const socket = socketIOClient("https://incredible-chatapp.herokuapp.com/")
+			const socket = socketIOClient()
 			return socket
 		case 'DISCONNECT_SOCKET':
 			state.off()
