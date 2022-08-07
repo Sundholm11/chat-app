e193f90735d6bf30175e886139a91c27ac413d80
Daniel Sundholm <danieljoonatan.sundholm@gmail.com>
2022-08-07 21:37:00 +0300

    New build

---
+++ client/build/precache-manifest.7487c79610b219ea0f40adea7a4606ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87026bc5770a69945197ae0ee1a21916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658a2a0ed9c3284e114a",
+    "url": "/static/js/2.89bb551b.chunk.js"
+  },
+  {
+    "revision": "9722af05b6b470c2989ebd17f8d2ee37",
+    "url": "/static/js/2.89bb551b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24dc16ea913aa03ecfad",
+    "url": "/static/js/main.d5776d88.chunk.js"
+  },
+  {
+    "revision": "3883dcfc88eefa164160",
+    "url": "/static/js/runtime-main.8270af98.js"
+  }
+]);
